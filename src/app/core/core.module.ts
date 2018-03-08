a5a9413dc4defd1e69b0defe81583b5d6614e213
Michael De Abreu <michaeljota@outlook.com>
2018-03-08 14:16:00 -0400

    feat(app): import noop router module in core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [NavbarComponent]
+  imports: [CommonModule, RouterModule.forChild([])],
+  declarations: [NavbarComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
