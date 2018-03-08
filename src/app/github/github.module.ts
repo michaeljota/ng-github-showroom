281e337c698439a433ad83a417b756ad813dae44
Michael De Abreu <michaeljota@outlook.com>
2018-03-08 14:32:25 -0400

    feat(github): create user list component

--- src/app/github/github.module.ts
+++ src/app/github/github.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { GithubRoutingModule } from './github-routing.module';
 
+import { UserListComponent } from './user-list/user-list.component';
+
 @NgModule({
   imports: [
     CommonModule,
     GithubRoutingModule
   ],
-  declarations: []
+  declarations: [UserListComponent]
 })
 export class GithubModule { }
