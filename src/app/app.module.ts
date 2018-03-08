c4604e3da2ff0ed529ab50bdcb07be110ba27475
Michael De Abreu <michaeljota@outlook.com>
2018-03-08 14:13:37 -0400

    feat(app): create routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
