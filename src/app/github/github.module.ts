c5a480401f8730088c6d01af194f462be5cf7da4
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 10:51:26 -0400

    feat(github): import shared module

--- src/app/github/github.module.ts
+++ src/app/github/github.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
 
 import { GithubRoutingModule } from './github-routing.module';
 
@@ -10,7 +11,7 @@ import { UsersService } from './shared/users.service';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     GithubRoutingModule
   ],
   declarations: [UserListComponent, UserCardComponent, PaginationBarComponent],
