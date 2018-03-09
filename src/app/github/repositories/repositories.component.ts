43f1121444ca428d2444308e07cb648bc96e3a44
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 15:57:52 -0400

    feat(github): update repositories component to sync the current page

--- src/app/github/repositories/repositories.component.ts
+++ src/app/github/repositories/repositories.component.ts
@@ -14,6 +14,7 @@ import { RepositoriesService } from '../shared/repositories.service';
 export class RepositoriesComponent implements OnInit {
   repositories: Observable<Repository[]>;
   totalPages: number;
+  currentPage: number;
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -36,6 +37,7 @@ export class RepositoriesComponent implements OnInit {
             }
             return +queryParamMap.get('page');
           }),
+          tap(page => (this.currentPage = page)),
         ),
       ),
       map(([repositories, virtualPage]) => {
@@ -47,6 +49,7 @@ export class RepositoriesComponent implements OnInit {
   }
 
   onPageChange(page: number) {
+    this.currentPage = page;
     // changes the route without moving from the current view
     this.router.navigate([], {
       relativeTo: this.route,
