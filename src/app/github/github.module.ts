a6dcc625cce2e58eff701c7b74fad1a28a5deeb8
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 10:44:55 -0400

    feat(github): create users service

--- src/app/github/github.module.ts
+++ src/app/github/github.module.ts
@@ -6,12 +6,14 @@ import { GithubRoutingModule } from './github-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserCardComponent } from './user-card/user-card.component';
 import { PaginationBarComponent } from './pagination-bar/pagination-bar.component';
+import { UsersService } from './shared/users.service';
 
 @NgModule({
   imports: [
     CommonModule,
     GithubRoutingModule
   ],
-  declarations: [UserListComponent, UserCardComponent, PaginationBarComponent]
+  declarations: [UserListComponent, UserCardComponent, PaginationBarComponent],
+  providers: [UsersService]
 })
 export class GithubModule { }
