4d1d510cafb0db8d509f5dfa88e88290ee8ddc97
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 11:33:29 -0400

    feat(github): update pagination bar component

--- src/app/github/pagination-bar/pagination-bar.component.ts
+++ src/app/github/pagination-bar/pagination-bar.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  ChangeDetectionStrategy,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pagination-bar',
   templateUrl: './pagination-bar.component.html',
-  styleUrls: ['./pagination-bar.component.scss']
+  styleUrls: ['./pagination-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginationBarComponent implements OnInit {
+export class PaginationBarComponent {
+  @Input() totalPages = 1;
+  @Input() arrowsOnly = false;
 
-  constructor() { }
+  @Output() pageChange = new EventEmitter<number>();
 
-  ngOnInit() {
+  private currentPage = 1;
+
+  /**
+   * Creates a new array to be iterated in.
+   */
+  public get pages(): Array<number> {
+    if (this.arrowsOnly) {
+      return [this.currentPage];
+    }
+    const length = this.totalPages;
+    return Array.from({ length }).map((_, i) => i + 1);
   }
 
+  onClickPage(page: number) {
+    this.currentPage = page;
+    this.pageChange.emit(page);
+  }
+
+  onClickPrevious() {
+    this.currentPage--;
+    this.pageChange.emit(this.currentPage);
+  }
+
+  onClickNext() {
+    this.currentPage++;
+    this.pageChange.emit(this.currentPage);
+  }
+
+  getAriaLabelForPage(page) {
+    return `Page ${page}`;
+  }
+
+  hasPrevious() {
+    return this.currentPage > 1;
+  }
+
+  hasNext() {
+    return this.arrowsOnly || this.currentPage < this.totalPages;
+  }
+
+  isPageActive(page) {
+    return this.currentPage === page;
+  }
 }
