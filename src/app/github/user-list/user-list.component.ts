4fa9f0c8eee2295f8fc8396bd30740bf75b355ce
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 11:39:10 -0400

    feat(github): update user list component to show a list of users

--- src/app/github/user-list/user-list.component.ts
+++ src/app/github/user-list/user-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { User } from '../shared/user.model';
+import { UsersService } from '../shared/users.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  users: Observable<User[]>;
 
-  constructor() { }
+  constructor(private readonly usersServices: UsersService) {}
 
   ngOnInit() {
+    this.users = this.usersServices.getAll();
   }
 
+  onPageChange(page: number) {
+    page = (page - 1 ) * 4;
+    console.log(page);
+    this.users = this.usersServices.getAll(page);
+  }
 }
