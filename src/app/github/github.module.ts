dca660ca71cdc925d3d40263cd8b3bf938e705cf
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 00:30:03 -0400

    feat(github): create pagination bar component

--- src/app/github/github.module.ts
+++ src/app/github/github.module.ts
@@ -5,12 +5,13 @@ import { GithubRoutingModule } from './github-routing.module';
 
 import { UserListComponent } from './user-list/user-list.component';
 import { UserCardComponent } from './user-card/user-card.component';
+import { PaginationBarComponent } from './pagination-bar/pagination-bar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     GithubRoutingModule
   ],
-  declarations: [UserListComponent, UserCardComponent]
+  declarations: [UserListComponent, UserCardComponent, PaginationBarComponent]
 })
 export class GithubModule { }
