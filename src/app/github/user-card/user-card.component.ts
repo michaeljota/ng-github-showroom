01be351c7ced215ce5160e0aed41bde79f5ae243
Michael De Abreu <michaeljota@outlook.com>
2018-03-09 11:34:36 -0400

    feat(github): update user card component to show user information

--- src/app/github/user-card/user-card.component.ts
+++ src/app/github/user-card/user-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../shared/user.model';
 
 @Component({
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+  styleUrls: ['./user-card.component.scss'],
 })
-export class UserCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class UserCardComponent {
+  @Input() user: User;
 }
