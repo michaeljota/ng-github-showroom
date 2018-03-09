dbd284759edf50f426de2029de9a41074b06ae60
Michael De Abreu <michaeljota@outlook.com>
2018-03-08 23:33:56 -0400

    feat(github): create routes to lazy load github module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'showroom',
+  },
+  {
+    path: 'showroom',
+    loadChildren: './github/github.module#GithubModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
